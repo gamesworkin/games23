9a436330bfd4bd3d062ec10425b6c26db86f679f
gamesworkin <gamesworkin@gmail.com>
2026-06-28 20:12:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"; 
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
 import { getDatabase, ref, set, get, onValue, update, remove } from "firebase/database";
 
